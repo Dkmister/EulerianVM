687423938f54dfc4d7be56d5517a7016db712c7c
dkmisterv <vilmar.neto@gmail.com>
2016-12-03 14:11:44 -0200

    Arquivo de entrada e saida finalizado

--- src/es.py
+++ src/es.py
@@ -41,3 +41,33 @@ def _carrega_video(nome_do_arquivo_video):
 def carrega_video_float(nome_do_arquivo_video):
     video_data, fps = _load_video(nome_do_arquivo_video)
     return uint8_2_float(video_data), fps
+
+def toca_video(nome_do_arquivo_video):
+    video_orig, fps = carrega_video_float(nome_do_arquivo_video)
+    toca_video_data(video_orig)
+
+def toca_piramide(piramide):
+    i = 0
+    while True:
+        try:
+            for nivel, vid in enumerate(piramide):
+                cv2.imshow('Nivel %i' % nivel, vid[i])
+            i = i + 1
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        except IndexError:
+            break
+
+def toca_video_data(frames):
+    toca_piramide([frames])
+
+
+def salva_video(video,fps,save_nome_arquivo='media/output.avi')
+
+    print (save_nome_arquivo)
+    video = float_2_uint8(video)
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    escritor = cv2.VideoWriter(save_nome_arquivo,fourcc,fps,(video.shape[2],video.shape[1]),1)
+    for i in range(0,video.shape[0]):
+        res = cv2.convertScaleAbs(video[i])
+        escritor.write(res)
